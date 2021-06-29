26d4b2d3971833d20ce0708dfcb348c104f01fee
Student <Student@Student-PC>
2021-06-29 09:13:14 +0800

    ss

---
+++ src/tw/kevin/Brad10X.java
@@ -0,0 +1,30 @@
+package tw.kevin;
+
+import java.util.Scanner;
+
+public class Brad10X {
+
+	public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
+	System.out.println("Month =");
+	int month = scanner.nextInt();
+	int days = 0;
+	
+	
+	switch (month) {
+	  case 1:case 3:case 5:case 7:case 8:case 10:case 12:
+		  days = 31;
+		  break;
+	  case 2:
+		  days = 28;
+		  break;
+	  case 4:case 6:case 9:case 11:
+		  days = 30;
+		  break;
+	
+	}
+	
+	System.out.println(String.format("%d月有%d天 " ,month, days));
+	}
+
+}

72d42d9988ca6601bc0f0061e959c24cbf8c13d9
Student <Student@Student-PC>
2021-06-29 11:57:17 +0800

    X*X multiplication table

--- src/tw/kevin/Brad12.java
+++ src/tw/kevin/Brad12.java
@@ -1,14 +1,27 @@
 package tw.kevin;
 
+import java.util.Scanner;
+
 public class Brad12 {
 
 	public static void main(String[] args) {
-		
-		int row	= 4;
-		int col = 3;
-		int start = 2;
+	//-------------------------------------
+		Scanner rowS =new Scanner(System.in);
+		System.out.println("Enter a number of row for your multiplication table: ");
+		int row = rowS.nextInt();
+	//-------------------------------------	
+		Scanner colS = new Scanner(System.in);
+		System.out.println(",then enter a number of column:  ");
+		int col = colS.nextInt();
+	//-------------------------------------	
+		Scanner startS = new Scanner(System.in);
+		System.out.println("Which number you want to start?: ");
+		int start = startS.nextInt();		
 		int end = row * col + start -1 ;
-				
+		
+		System.out.println(String.format("\nCongratulation!! you get the multiplication table from "
+				+ "\n%d x 1 to %d x %d\n"
+				+ "quite easy isn't it?",start,end,end));
 		System.out.println("------------------------------------------------------------");
 		for (int k = 0; k < row; k++) {
 			for (int j = 1; j <= end ; j++) {

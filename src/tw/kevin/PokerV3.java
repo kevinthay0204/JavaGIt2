88b065e9cba5e2fbaae4d8c5c019d34180850458
Student <Student@Student-PC>
2021-06-30 16:12:34 +0800

    cards in 4 pines

--- src/tw/kevin/PokerV3.java
+++ src/tw/kevin/PokerV3.java
@@ -16,11 +16,11 @@ public class PokerV3 {
 			poker[rand]=temp; // System.out.println(temp); 洗牌交換
 	
 		}
-			for (int v:poker) {
-				System.out.println(v);
-			}
-			System.out.println("System.currentTimeMillis()");
-		System.out.println(System.currentTimeMillis()-start);
+//			for (int v:poker) {				// 順序洗牌1~52張都擺出來0 1 2 3...0 1 2 3這樣發
+//				System.out.println(v);
+//			}
+//			System.out.println("System.currentTimeMillis()");
+//		System.out.println(System.currentTimeMillis()-start);
 		
 		//------------------------------ 發排到四堆
 		int[][] players = new int[4][13];
@@ -28,17 +28,27 @@ public class PokerV3 {
 			players[i%4][i/4] = poker[i];
 		}
 		
-		for (int[] player :players) {
-			for (int card :player) {				
-				System.out.print(card+ " ");
-			}
-			System.out.println();
-			}
-			
+		String[] suits= {"黑桃", "紅心", "方塊", "梅花"};
+		String[] values = { "A", "2", "3", "4","5", "6"
+				,"7", "8" ,"9", "10","J", "Q","K"};
+		String[] ppplayer = {"1","2","3","4"}; //自己家的
 		
-	}
+		
+		
+		for (int[] player :players) {
+			
+			for (int card :player) {
 
-}
+			System.out.print(suits[card/13]+"-"+values[card%13]+" ");}
+			System.out.println();
+		}
+			}
+	}
+	
+		
+	
+
+
 
 //for (int i=1; i<5;i++) {
 //	System.out.println("玩家"+i);

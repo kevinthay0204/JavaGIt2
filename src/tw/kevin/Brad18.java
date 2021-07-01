865c891d641204c45d18e11fe018fd8a1e061033
Student <Student@Student-PC>
2021-07-01 09:07:14 +0800

    do while example

---
+++ src/tw/kevin/Brad18.java
@@ -0,0 +1,18 @@
+package tw.kevin;
+
+public class Brad18 {
+
+	public static void main(String[] args) {
+		// do =while 至少做一次才去判斷
+		
+		int i=11;
+				
+		do {
+			System.out.println(i++);
+		}while(i<10);
+		
+		
+		
+	}
+	
+}
